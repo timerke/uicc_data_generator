e4828262a336f297cc79e353d1accd308877423d
timerke <timerke@mail.ru>
2024-08-05 14:20:42 +0300

    Fixed readme

--- scripts/release.bat
+++ scripts/release.bat
@@ -9,19 +9,14 @@ if exist venv rd /S /Q venv
 venv\Scripts\python -m pip install --upgrade pip
 venv\Scripts\python -m pip install -r requirements.txt
 venv\Scripts\python -m pip install pyinstaller
-venv\Scripts\pyinstaller run_server.py --clean --onefile ^
---add-data "gcodebin\gcodebin.json;gcodebin" ^
---add-data "http_server\static\css\*;http_server\static\css" ^
---add-data "http_server\static\img\*;http_server\static\img" ^
---add-data "http_server\static\js\*;http_server\static\js" ^
---add-data "http_server\views\*;http_server\views" ^
---icon "http_server\static\img\favicon.ico"
+venv\Scripts\pyinstaller run.py --clean --onefile ^
+--add-data "uiccgenerator\apdu.json;uiccgenerator" ^
+--icon "img\icon.ico"
 
-copy config.ini dist
-copy README_RELEASE.md dist\README.md
+copy README.md dist
 rename dist release
 cd release
-rename run_server.exe gcodebin_server.exe
+rename run.exe uicc_generator.exe
 cd ..
 if exist build rd /S /Q build
 if exist dist rd /S /Q dist
