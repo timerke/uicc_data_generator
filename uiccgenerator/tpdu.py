43c340575b5b09287476d31beb56c52b7fb56ddc
timerke <timerke@mail.ru>
2024-08-07 13:57:47 +0300

    Changed docstring

--- uiccgenerator/tpdu.py
+++ uiccgenerator/tpdu.py
@@ -4,32 +4,32 @@ class TPDU:
     """
 
     @staticmethod
-    def _map_for_t0(data: bytes, message_case: int) -> bytes:
+    def _map_for_t0(apdu_data: bytes, message_case: int) -> bytes:
         """
-        :param data:
-        :param message_case:
+        :param apdu_data: bytes after APDU;
+        :param message_case: message case.
         :return:
         """
 
         if message_case == 1:
             p3 = 0
-            return data + p3.to_bytes(1, "big")
+            return apdu_data + p3.to_bytes(1, "big")
 
         if message_case == 4:
-            return data[:-1]
+            return apdu_data[:-1]
 
-        return data
+        return apdu_data
 
-    def map_bytes_from_apdu(self, data: bytes, message_case: int, t0: bool = False) -> bytes:
+    def map_bytes_from_apdu(self, apdu_data: bytes, message_case: int, t0: bool = False) -> bytes:
         """
-        :param data:
-        :param message_case:
+        :param apdu_data: bytes after APDU;
+        :param message_case: message case;
         :param t0: if True, then the character based transmission protocol should be used. Otherwise, the block based
         transmission protocol will be used.
-        :return:
+        :return: bytes mapped to TPDU.
         """
 
         if t0:
-            return self._map_for_t0(data, message_case)
+            return self._map_for_t0(apdu_data, message_case)
 
-        return data
+        return apdu_data
